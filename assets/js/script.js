516e52ed8054a4626d281c8774945daeb0bcd255
samlaubscher <42819957+samlaubscher@users.noreply.github.com>
2020-02-27 19:27:24 +0000

    Initially create star instances and show them in the canvas

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,11 +10,39 @@ function initialise_scripts(){
     var cnvsHeight = window.innerHeight -4; // ^^^^^
     ctx.canvas.width = cnvsWidth; // sets dimensions to these variables 
     ctx.canvas.height = cnvsHeight; // ^^^^ 
+    
+    var numberOfStars = 200; // how many stars will be generated
+    var starsArray = []; // creates an array to store the star object instances
+    var size = 1;
+    for(var i = 0; i < numberOfStars; i++){ // maintains star instances to the defined number of stars
+        starsArray[i] = new generate_star(); // generates new star object per array iteration
+    }
+
+
+
+function generate_star(){
+    this.x = Math.random()*canvas.width; // generates random position on the canvas width
+    this.y = Math.random()*canvas.height; // generates random position on the canvas height
+    this.z = Math.random()*canvas.width; // generates random position on the canvas depth relative
+
+    this.show = function(){
+        ctx.beginPath();
+        ctx.fillStyle = "#fff";
+        ctx.arc(this.x, this.y, size, 0, Math.PI*2);
+        ctx.fill(); // generates star object
+    }
+}
+
+
 
 
 function draw(){
     ctx.fillStyle = "#000";
     ctx.fillRect(0, 0, canvas.width, canvas.height); // draws black background each frame
+    
+    for(var i = 0; i < numberOfStars; i++){ 
+        starsArray[i].show(); // shows star objects per array iteration
+    }
 }
 
 function update(){ // defines what happens when update is called at bottom
