b1cfa0701c22e61520f046a5b8ae8f5a3dc0b3cb
samlaubscher <42819957+samlaubscher@users.noreply.github.com>
2020-02-27 17:31:51 +0000

    Add canvas screen sizing and colour properties

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,13 @@
 window.onload = function(){ // makes sure window is loaded first before running
     initialise_scripts(); // runs the full scripts function
+    window.addEventListener("resize", initialise_scripts, false); // restarts the function on resize
 }
 
 function initialise_scripts(){
     var canvas = document.getElementById("canvas");
     var ctx = canvas.getContext("2d");
+    var cnvsWidth = window.innerWidth -4; //sets the sizes to inner window sizes. -4 removes scroll bar
+    var cnvsHeight = window.innerHeight -4; // ^^^^^
+    ctx.canvas.width = cnvsWidth; // sets dimensions to these variables 
+    ctx.canvas.height = cnvsHeight; // ^^^^ 
 }
