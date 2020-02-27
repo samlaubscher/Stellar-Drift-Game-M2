92b5898704638d51618c1ac8447becf974241b86
samlaubscher <42819957+samlaubscher@users.noreply.github.com>
2020-02-27 05:32:14 +0000

    Add canvas

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+window.onload = function(){ // makes sure window is loaded first before running
+    initialise_scripts(); // runs the full scripts function
+}
+
+function initialise_scripts(){
+    var canvas = document.getElementById("canvas");
+    var ctx = canvas.getContext("2d");
+}
